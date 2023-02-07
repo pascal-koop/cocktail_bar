eb57530259db8de39a854d3d26e811b96450d373
Pascal Koop <pk@megorei.com>
2023-02-07 16:50:23 +0100

    Add fullscreen cart and information modals

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,6 +4,30 @@ import { aliases, fa } from 'vuetify/iconsets/fa';
 import { mdi } from 'vuetify/iconsets/mdi';
 import 'vuetify/styles';
 
+const customDarkTheme = {
+  dark: true,
+  colors: {
+    background: '#15202b',
+    surface: '#15202b',
+    primary: '#3f51b5',
+    secondary: '#03dac6',
+    error: '#f44336',
+    info: '#2196F3',
+    success: '#4caf50',
+    warning: '#fb8c00',
+  },
+};
+
+const customLightTheme = {
+  dark: false,
+  colors: {
+    background: "#212121",
+    surface: "#15202b",
+    primary: "#3f51b5",
+    secondary: "E57373",
+    error: "#ffcc00",
+  },
+};
 export default createVuetify({
   icons: {
     defaultSet: 'fa',
@@ -14,6 +38,10 @@ export default createVuetify({
     },
   },
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'customDarkTheme',
+    themes: {
+      customDarkTheme,
+      customLightTheme,
+    },
   },
 });
