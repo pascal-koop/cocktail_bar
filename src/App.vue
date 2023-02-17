e05c3e434d92838f002e19b086e0c8484a2f64ce
Pascal Koop <pk@megorei.com>
2023-02-17 15:58:41 +0100

    finish creating signup form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <v-app>
+
     <RouterView name="default" v-slot="{ Component }">
       <transition name="route" mode="out-in" appear>
         <component :is="Component" />
       </transition>
     </RouterView>
-
-    
-    
     <RouterView name="BottomNavigationBar"></RouterView>
   </v-app>
 </template>
@@ -17,16 +15,5 @@ import DarkLightMode from './components/LightDarkSwitcher.vue';
 </script>
 
 <style>
-.route-enter-from {
-  opacity: 0;
-  transform: translateX(-100px);
-}
-.route-enter-active,
-.route-leave-active {
-  transition: all 0.2s ease-out;
-}
-.route-leave-to {
-  opacity: 0;
-  transform: translateX(100px);
-}
+
 </style>
